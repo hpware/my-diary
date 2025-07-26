49f77f8db4c57f8c8109e3f036c960d201a0a299
吳元皓 <hw@yuanhau.com>
2025-07-26 22:37:23 +0800

    Switch to python

---
+++ app.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from flask import Flask, render_template
+
+load_dotenv()
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')

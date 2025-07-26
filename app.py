d1b7974c24e3a304e08f89f6ca663153c31f81a4
吳元皓 <hw@yuanhau.com>
2025-07-26 22:45:17 +0800

    Doin stuff.

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/<slug>")
+def diraryPage(slug):
+    return render_template("dirary_template.html", content="# Here is the markdown content yo", title=slug)
